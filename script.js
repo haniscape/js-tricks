79cd9455fb8f5ff8ed1e73035bbecbce602dc087
Hany Afifi <hanyafifi@gmail.com>
2020-11-03 00:03:57 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Hello JavaScript
+
+
+
+/* In-LINE COMMENT 
+* 2 forward slashes
+* "in-line" means it can be in the middle of a line of code
+var number = 5;  // in-line comment
+*/
+
+
+/* MULTILINE COMMENT
+* slash star ... star slash
+multi-line comments in VS Code ---> SHiFT + ALt + A
+*/
+
